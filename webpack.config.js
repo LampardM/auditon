b186acdb64da0276ed17dd187df618dbcf8ca7a7
张龙 <zhangl@jiagouyun.com>
2018-05-02 13:42:16 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  entry: {
+    vendor: [
+      'jquery',
+      'echarts/lib/echarts',
+      'echarts/lib/chart/bar',
+      'echarts/lib/chart/line',      
+      'echarts/lib/component/tooltip',
+      'echarts/lib/component/title',
+      'echarts/lib/component/legend'
+    ],
+    auditon: path.resolve(__dirname, 'src/js/auditon.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'less-loader',
+        }],
+      },
+      {
+        test: /\.json$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'url-loader?limit=10000',
+          'img-loader',
+        ],
+      },
+      {
+        test: /\.(webm|mp4)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      _: 'lodash'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/auditon.html'),
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        html5: true,
+        minifyCSS: true,
+        removeComments: true,
+        removeEmptyAttributes: true
+      },
+      chunks: ['vendor', 'auditon'],
+    })
+  ],
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+module.exports = config
